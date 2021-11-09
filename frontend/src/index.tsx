facf5992590aeb03a33161693c2ecda66101656a
Fatima Camero <fcamero12@gmail.com>
2021-11-08 21:31:52 -0800

    add redux toolkit and fix dependencies

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import { SocketContext, socket } from './context/socket';
 import './index.css';
 import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
-  <SocketContext.Provider value={socket}>
-    <App />
-  </SocketContext.Provider>
-  , document.getElementById('root')
+  <Provider store={store}>
+    <SocketContext.Provider value={socket}>
+      <App />
+    </SocketContext.Provider>
+  </Provider>, 
+  document.getElementById('root')
 );
 
