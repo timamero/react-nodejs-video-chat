97f4ab3fc9162b07d81f94299137b3682ad391fb
Fatima Camero <fcamero12@gmail.com>
2021-11-28 18:50:43 -0800

    send invite request event to invitee

--- backend/src/controllers/privateChatHandler.ts
+++ backend/src/controllers/privateChatHandler.ts
@@ -3,6 +3,9 @@ import { Server, Socket } from 'socket.io';
 const privateChatHandler = (socket: Socket, io: Server) => {
   socket.on('invite private chat', (inviteeId) => {
     console.log(`${socket.id} invited ${inviteeId} to a chat`)
+
+    // Send invite request with inviter id to invitee
+    io.to(inviteeId).emit('invite requested', socket.id)
   })
 
   // socket.on('disconnect', () => {
