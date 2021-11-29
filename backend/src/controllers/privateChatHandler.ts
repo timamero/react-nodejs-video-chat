0160383480003999a2009df97696becfe57ce35f
Fatima Camero <fcamero12@gmail.com>
2021-11-28 19:26:33 -0800

    add event listener for invite accepted

--- backend/src/controllers/privateChatHandler.ts
+++ backend/src/controllers/privateChatHandler.ts
@@ -8,6 +8,22 @@ const privateChatHandler = (socket: Socket, io: Server) => {
     io.to(inviteeId).emit('invite requested', socket.id)
   })
 
+  socket.on('invite accepted', (inviterId) => {
+    console.log(`${socket.id} accepted chat with ${inviterId}`)
+
+    // Add invitee and inviter join a private room
+    const privateRoom = `${inviterId}-room`
+    io.in(socket.id).socketsJoin(privateRoom)
+    io.in(inviterId).socketsJoin(privateRoom)
+
+    // Send invite request with inviter id to invitee
+    io.to(privateRoom).emit('enter chat room', privateRoom)
+  })
+
+  // socket.on('invite rejected', () => {
+
+  // })
+
   // socket.on('disconnect', () => {
 
   // })
