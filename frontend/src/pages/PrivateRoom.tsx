d5655129f008b40b72517aa061edd2d3b309fcf2
Fatima Camero <fcamero12@gmail.com>
2021-12-17 14:19:05 -0800

    add notification after user is redirected from room they don't have access too

--- frontend/src/pages/PrivateRoom.tsx
+++ frontend/src/pages/PrivateRoom.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import { useAppSelector } from '../app/hooks';
+import { setNotification, resetNotification } from '../app/features/notificationSlice';
+import { useAppSelector, useAppDispatch } from '../app/hooks';
 import { useNavigate } from 'react-router-dom';
 
 const PrivateRoom = () => {
   const navigate = useNavigate()
+  const dispatch = useAppDispatch()
   const room = useAppSelector(state => state.room)
   const userId = useAppSelector(state => state.user.id)
 
   const userHasAccess = room.users.includes(userId)
 
   if (!userHasAccess) {
+    
+    const notificationData = {
+      notificationContent: 'You do not have access to this room.',
+      notificationType: 'is-warning',
+      isLoading: false,
+      isActive: true,
+      }
+    dispatch(setNotification(notificationData))
     navigate('/')
+    setTimeout(() => dispatch(resetNotification()), 5000)
   }
 
   // if user leaves room and comes back, get room data and recheck if user has access, use a useEffect
