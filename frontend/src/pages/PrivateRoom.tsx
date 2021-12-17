07670ff5240316bfc51685cfc866c3f52ef42a4d
Fatima Camero <fcamero12@gmail.com>
2021-12-17 14:10:39 -0800

    redirect user to home when they do not have access to room

--- frontend/src/pages/PrivateRoom.tsx
+++ frontend/src/pages/PrivateRoom.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import { useAppSelector } from '../app/hooks';
+import { useNavigate } from 'react-router-dom';
 
 const PrivateRoom = () => {
+  const navigate = useNavigate()
   const room = useAppSelector(state => state.room)
   const userId = useAppSelector(state => state.user.id)
 
   const userHasAccess = room.users.includes(userId)
+
+  if (!userHasAccess) {
+    navigate('/')
+  }
 
   // if user leaves room and comes back, get room data again, use a useEffect
 
