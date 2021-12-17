c41214ff8c50cfb1dc194f061db8ef3fafe34024
Fatima Camero <fcamero12@gmail.com>
2021-12-17 13:56:09 -0800

    update room to check if user has access

--- frontend/src/pages/PrivateRoom.tsx
+++ frontend/src/pages/PrivateRoom.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import { useAppSelector } from '../app/hooks';
 
 const PrivateRoom = () => {
+  const room = useAppSelector(state => state.room)
+  const userId = useAppSelector(state => state.user.id)
+
+  const userHasAccess = room.users.includes(userId)
+
+  // if user leaves room and comes back, get room data again, use a useEffect
+
   return (
     <Layout>
       Private chat room
+      
+      {userHasAccess ? <p>User has access</p> : <p>No access</p>}
     </Layout>
   )
 }
