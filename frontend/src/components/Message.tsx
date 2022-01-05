7747742037409b76a622c4fd91a942f833e189ff
Fatima Camero <fcamero12@gmail.com>
2022-01-04 16:20:09 -0800

    memoize message component so it is not rerendered each time a message is sent

--- frontend/src/components/Message.tsx
+++ frontend/src/components/Message.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { memo } from "react";
 
 interface MessageProps {
   message: string;
 }
 
-const Message: React.FC<MessageProps> = ({ message }) => {
+const Message: React.FC<MessageProps> = memo(({ message }) => {
+  console.log('message rendered', message)
   return (
     <div className="message box">
       {message}
     </div>
   )
-}
+})
 
 export default Message;
