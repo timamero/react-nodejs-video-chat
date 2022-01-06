8fc45ecfb4658728a766be15aaac9fdcf6f8dfbf
Fatima Camero <fcamero12@gmail.com>
2022-01-05 23:27:22 -0800

    refactor socket listeners

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useCallback } from 'react';
 import {
   BrowserRouter as Router,
   Routes,
@@ -22,72 +22,89 @@ const App: React.FC = () => {
   const socket = useContext(SocketContext)
 
   const activeUsers = useAppSelector(state => state.activeUsers.users)
-  console.log('app - active user:', activeUsers)
+  // console.log('app - active user:', activeUsers)
+
+  const handleSetNewUser = useCallback((usernameFromLocalStorage) => {
+    dispatch(setNewUser(usernameFromLocalStorage))
+  }, [dispatch])
+
+  const handleAddUsers = useCallback((users: User[]) => {
+    // Get list of active users from server
+    console.log('receiving user list', users) // repeated three times
+    dispatch(getAllActiveUsers(users))
+  }, [dispatch])
+
+  const handleSetId = useCallback(id => {
+    // Setting the socket ID as the current user's ID
+    console.log('setting app user id')
+    dispatch(setId(id))
+  }, [dispatch])
+
+  const handleInviteRequested = useCallback(inviterId => {
+    console.log('invite from ', inviterId)
+    const inviter = activeUsers.find((user: User) => user.id === inviterId)
+
+    if (inviter) {
+        const modalData = {
+        modalContent: `${inviter.username} has invited you to a private chat?`,
+        confirmBtnText: 'Yes, accept invite.',
+        declineBtnText: 'No, decline invite.',
+        isActive: true,
+        peerId: inviterId,
+        socketEvent: 'invite requested'
+      }
+      dispatch(setModal(modalData))
+    } 
+  }, [dispatch, activeUsers])
+
+  const handleInviteDeclined = useCallback(inviteeId => {
+    console.log('invitation to chat was declined by', inviteeId)
+    const peer = activeUsers.find((user: User) => user.id === inviteeId)
+    if (peer) {
+      const notificationData = {
+      notificationContent: `${peer.username} is not able to chat`,
+      notificationType: 'is-warning',
+      isLoading: false,
+      isActive: true,
+      }
+      dispatch(setNotification(notificationData))
+      setTimeout(() => dispatch(resetNotification()), 5000)
+    }
+  }, [dispatch, activeUsers])
 
   useEffect(() => {
+    console.log('local storage useEffect')
     const usernameFromLocalStorage = window.localStorage.getItem('chat-username')
 
     // If username is found in localStorage, 
     // set the current user's username to the one found in localStorage
     if (usernameFromLocalStorage) {
+      console.log('getting user from local storage')
       socket.emit('user entered', usernameFromLocalStorage)
-      dispatch(setNewUser(usernameFromLocalStorage))
+      handleSetNewUser(usernameFromLocalStorage)
     }
-  }, [dispatch, socket])
+
+  }, [socket, handleSetNewUser])
 
   useEffect(() => {
-    console.log('socket useEffect')
-    // Manually connect to socket
-    socket.connect()
-
     /*
     * Socket event listeners
     */
     socket.once('connect', () => {
       console.log('Connected to server')
     })
-    socket.on('get user list', users => {
-      // Get list of active users from server
-      console.log('receiving user list', users)
-      dispatch(getAllActiveUsers(users))
-    })
-    socket.on('get socket id', id => {
-      // Setting the socket ID as the current user's ID
-      console.log('setting app user id')
-      dispatch(setId(id))
-    })
-    socket.once('invite requested', inviterId => {
-      console.log('invite from ', inviterId)
-      const inviter = activeUsers.find((user: User) => user.id === inviterId)
+    socket.on('get user list', handleAddUsers)
+    socket.on('get socket id', handleSetId)
+    socket.on('invite requested', handleInviteRequested)
+    socket.on('invite declined', handleInviteDeclined)
 
-      if (inviter) {
-          const modalData = {
-          modalContent: `${inviter.username} has invited you to a private chat?`,
-          confirmBtnText: 'Yes, accept invite.',
-          declineBtnText: 'No, decline invite.',
-          isActive: true,
-          peerId: inviterId,
-          socketEvent: 'invite requested'
-        }
-
-        dispatch(setModal(modalData))
-      } 
-    })
-    socket.on('invite declined', (inviteeId) => {
-      console.log('invitation to chat was declined by', inviteeId)
-      const peer = activeUsers.find((user: User) => user.id === inviteeId)
-      if (peer) {
-        const notificationData = {
-        notificationContent: `${peer.username} is not able to chat`,
-        notificationType: 'is-warning',
-        isLoading: false,
-        isActive: true,
-        }
-        dispatch(setNotification(notificationData))
-        setTimeout(() => dispatch(resetNotification()), 5000)
-      } 
-    })
-  })
+    return () => {
+      socket.off('get user list', handleAddUsers)
+      socket.off('get socket id', handleSetId)
+      socket.off('invite requested', handleInviteRequested)
+      socket.off('invite declined', handleInviteDeclined)
+    }
+  }, [socket, handleAddUsers, handleSetId, handleInviteRequested, handleInviteDeclined])
  
   return (
     <Router>
