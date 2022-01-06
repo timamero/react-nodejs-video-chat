1d5e9abf9ed419ef3db13c3bc4e37c4e9d74c45d
Fatima Camero <fcamero12@gmail.com>
2022-01-05 22:21:05 -0800

    moved socket connection method to socket file to prevent multiple event firing

--- frontend/src/context/socket.tsx
+++ frontend/src/context/socket.tsx
@@ -3,4 +3,5 @@ import { io, Socket } from "socket.io-client";
 
 // Create Socket instance and make it globally available
 export const socket: Socket = io("ws://localhost:3001");
+socket.connect()
 export const SocketContext = createContext(socket);
