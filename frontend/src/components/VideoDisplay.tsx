bc0e80383ea3a6159cb8fef127a93d445a737898
Fatima Camero <fcamero12@gmail.com>
2022-01-07 08:24:31 -0800

    add video elements

--- frontend/src/components/VideoDisplay.tsx
+++ frontend/src/components/VideoDisplay.tsx
@@ -3,8 +3,9 @@ import React from "react";
 const VideoDisplay = () => {
 
   return (
-    <div>
-      <p>Video placeholder</p>
+    <div className="is-flex is-flex-direction-row">
+      <video id="videoStream" autoPlay>There is a problem playing the video.</video>
+      <video id="remoteVideoStream" autoPlay>There is a problem playing the video.</video>
     </div>
   )
 }
