41f79800ba7a04f67d7a34976994cb3e46f9af40
Fatima Camero <fcamero12@gmail.com>
2022-01-07 11:31:52 -0800

    create myPeerConnection and webcamStream with useRef

--- frontend/src/components/VideoDisplay.tsx
+++ frontend/src/components/VideoDisplay.tsx
@@ -1,15 +1,17 @@
-import React, { useEffect, useContext, useRef, useCallback, useState, useMemo } from "react";
+import React, { useEffect, useContext, useRef, useCallback, useMemo } from "react";
 import { SocketContext } from "../context/socket";
 
 const VideoDisplay = () => {
   const socket = useContext(SocketContext);
 
-  const [webcamStream, setWebcamStream] = useState<MediaStream|null>(null)
+  const webcamStreamRef = useRef<MediaStream|null>(null)
+  const myPeerConnectionRef = useRef<RTCPeerConnection|null>(null)
 
   const streamRef = useRef<HTMLVideoElement|null>(null);
   const remoteStreamRef = useRef<HTMLVideoElement|null>(null);
 
-  let myPeerConnection: RTCPeerConnection | null = null;    // RTCPeerConnection
+  // let myPeerConnection: RTCPeerConnection | null = null   // RTCPeerConnection
+  // let myPeerConnection: RTCPeerConnection | null   // RTCPeerConnection
 
   const mediaConstraints = useMemo(() => {
     return {audio: true,
@@ -17,28 +19,47 @@ const VideoDisplay = () => {
     }
   }, [])
 
+  const createPeerConnection = useCallback( async () => {
+    myPeerConnectionRef.current = new RTCPeerConnection() // For peers to connect from different networks, need to specify TURN or STUN servers
+    
+    // Set up event handlers for the ICE negotiation process.
+    myPeerConnectionRef.current.onicecandidate = handleICECandidateEvent;
+    myPeerConnectionRef.current.oniceconnectionstatechange = handleICEConnectionStateChangeEvent;
+    myPeerConnectionRef.current.onicegatheringstatechange = handleICEGatheringStateChangeEvent;
+    myPeerConnectionRef.current.onsignalingstatechange = handleSignalingStateChangeEvent;
+    myPeerConnectionRef.current.onnegotiationneeded = handleNegotiationNeededEvent;
+    myPeerConnectionRef.current.ontrack = handleTrackEvent;
+  }, 
+  [
+    handleICECandidateEvent,
+    handleICEConnectionStateChangeEvent,
+    handleICEGatheringStateChangeEvent,
+    handleSignalingStateChangeEvent,
+    handleNegotiationNeededEvent,
+  ])
+
   const startVideoChat = useCallback( async () => {
-    if (!myPeerConnection) {
+    if (!myPeerConnectionRef.current) {
       createPeerConnection();
     }
 
     // Get access to webcam stream and display it in local stream
     try {
-      const localWebcamStream = await navigator.mediaDevices.getUserMedia(mediaConstraints)
-      setWebcamStream(localWebcamStream)
-      webcamStream?.getTracks().forEach((track: MediaStreamTrack) => {
-        if (myPeerConnection) {
-          myPeerConnection.addTrack(track, webcamStream)
+      webcamStreamRef.current = await navigator.mediaDevices.getUserMedia(mediaConstraints)
+      webcamStreamRef.current?.getTracks().forEach((track: MediaStreamTrack) => {
+        if (myPeerConnectionRef.current) {
+          myPeerConnectionRef.current.addTrack(track, webcamStreamRef.current!)
         }
       })
       if (streamRef.current) {
-        streamRef.current.srcObject = webcamStream;
+        streamRef.current.srcObject = webcamStreamRef.current;
       }
     } catch (err) {
       console.log('error in enterVideoChat - local webcam stream', err)
     }
-  }, [createPeerConnection, mediaConstraints, myPeerConnection, webcamStream])
+  }, [createPeerConnection, mediaConstraints])
 
+  
   useEffect(() => {
     socket.on('userWaiting', message => {
       console.log('message', message)
@@ -49,31 +70,32 @@ const VideoDisplay = () => {
       startVideoChat()
     })
 
-    socket.on('getVideoChatOffer', (sdp: RTCSessionDescription) => {
-      handleVideoChatOffer(sdp);
-    })
-
-    socket.on('getVideoChatAnswer', (sdp: RTCSessionDescription) => {
-      handleVideoChatAnswer(sdp)
-    })
-
-    socket.on('getCandidate', (candidate: RTCIceCandidate) => {
-      handleNewICECandidate(candidate)
-    })
   }, [socket, startVideoChat, handleVideoChatOffer, handleVideoChatAnswer, handleNewICECandidate])
   
 
-  async function createPeerConnection() {   
-    myPeerConnection = new RTCPeerConnection() // For peers to connect from different networks, need to specify TURN or STUN servers
+  socket.on('getVideoChatOffer', (sdp: RTCSessionDescription) => {
+    handleVideoChatOffer(sdp);
+  })
 
-    // Set up event handlers for the ICE negotiation process.
-    myPeerConnection.onicecandidate = handleICECandidateEvent;
-    myPeerConnection.oniceconnectionstatechange = handleICEConnectionStateChangeEvent;
-    myPeerConnection.onicegatheringstatechange = handleICEGatheringStateChangeEvent;
-    myPeerConnection.onsignalingstatechange = handleSignalingStateChangeEvent;
-    myPeerConnection.onnegotiationneeded = handleNegotiationNeededEvent;
-    myPeerConnection.ontrack = handleTrackEvent;
-  }
+  socket.on('getVideoChatAnswer', (sdp: RTCSessionDescription) => {
+    handleVideoChatAnswer(sdp)
+  })
+
+  socket.on('getCandidate', (candidate: RTCIceCandidate) => {
+    handleNewICECandidate(candidate)
+  })
+
+  // async function createPeerConnection() {   
+  //   myPeerConnection = new RTCPeerConnection() // For peers to connect from different networks, need to specify TURN or STUN servers
+
+  //   // Set up event handlers for the ICE negotiation process.
+  //   myPeerConnection.onicecandidate = handleICECandidateEvent;
+  //   myPeerConnection.oniceconnectionstatechange = handleICEConnectionStateChangeEvent;
+  //   myPeerConnection.onicegatheringstatechange = handleICEGatheringStateChangeEvent;
+  //   myPeerConnection.onsignalingstatechange = handleSignalingStateChangeEvent;
+  //   myPeerConnection.onnegotiationneeded = handleNegotiationNeededEvent;
+  //   myPeerConnection.ontrack = handleTrackEvent;
+  // }
 
   async function handleNegotiationNeededEvent() {
     if (myPeerConnection) {
