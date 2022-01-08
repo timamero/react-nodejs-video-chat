24a99e9e614bc5dde5a7651c37f6ce7589d9046f
Fatima Camero <fcamero12@gmail.com>
2022-01-07 21:57:15 -0800

    add socket subscribers and publishers for video chat to privateChatHandler

--- backend/src/controllers/privateChatHandler.ts
+++ backend/src/controllers/privateChatHandler.ts
@@ -1,5 +1,8 @@
 import { Server, Socket } from 'socket.io';
 
+let peer1: string;
+let peer2: string;
+
 const privateChatHandler = (socket: Socket, io: Server) => {
   socket.on('invite private chat', (inviteeId) => {
     console.log(`${socket.id} invited ${inviteeId} to a chat`)
@@ -33,6 +36,47 @@ const privateChatHandler = (socket: Socket, io: Server) => {
     
     io.to(sentMessageData.roomId).emit('receive chat message', messageData);
     console.log('message: ' + messageData.msg);
+  })
+
+  socket.on('start video request', (users: string[]) => {
+    peer1 = socket.id
+    peer2 = users.find(user => user !== socket.id)!
+    
+    console.log('start video request')
+
+    if (peer2) {
+      io.to(peer2).emit('start video invite')
+    }
+  })
+
+  socket.on('video request accepted', () => {
+    console.log('send video ready to ', peer1)
+    io.to(peer1).emit('video ready')
+  })
+
+  socket.on('video offer', ({sdp}) => {
+    console.log(`send get video offer to ${peer2}`)
+    
+    if (socket.id === peer1) {
+      io.to(peer2).emit('get video offer', sdp)
+    }
+  })
+
+  socket.on('video answer', ({sdp}) => {
+    console.log(`answer - send get video answer to ${peer1}`)
+    io.to(peer1).emit('get video answer', sdp)
+  })
+
+  socket.on('candidate', ({candidate}) => {
+    console.log(`ice candidate from ${socket.id}`)
+    
+    if (socket.id === peer1) {
+      console.log('send get candidate to peer2')
+      io.to(peer2).emit('get candidate', candidate)
+    } else {
+      console.log('send get candidate to peer1')
+      io.to(peer1).emit('get candidate', candidate)
+    }
   })
 
   socket.on('end chat', (roomId) => {
