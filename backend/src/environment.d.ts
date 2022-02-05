6e338c52181086eb91cdd8fa844a892a7c1f02ee
Fatima Camero <fcamero12@gmail.com>
2022-02-05 15:42:48 -0800

    extend node typing for process.env variables

---
+++ backend/src/environment.d.ts
@@ -0,0 +1,14 @@
+/*
+ * Extend node typings
+ *
+ * Resources:
+ * https://www.typescriptlang.org/docs/handbook/declaration-files/templates/global-d-ts.html
+ * https://dev.to/isthatcentered/typing-process-env-and-dealing-with-nodeenv-3ilm
+*/
+declare namespace NodeJS
+{
+    export interface ProcessEnv
+    {
+        MONGODB_URI: string;
+    }
+}
