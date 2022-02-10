cae63584192bfe0c8cd81a0bcb52a32acf96458e
Fatima Camero <fcamero12@gmail.com>
2022-02-10 06:39:25 -0800

    move connection to mongodb to app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,33 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
 
 const app = express();
 app.use(cors());
+
+/*
+ * Access variables in the .env file via process.env
+*/
+dotenv.config();
+
+
+/*
+ * Connect to MongoDB database
+*/
+const uri = process.env.MONGODB_URI
+export const client = new MongoClient(uri)
+const main = async () => {
+  try {
+    await client.connect()
+
+  } catch(e) {
+    console.log(e)
+  } finally {
+    await client.close()
+  }
+}
+
+main().catch(console.error)
 
 export default app;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,55 +1,11 @@
 import app from './app';
 import http from 'http';
-import dotenv from 'dotenv';
-import { MongoClient } from 'mongodb';
 import { Server } from 'socket.io';
 import video from './pubsub/video';
 import user from './pubsub/users';
 import privateChat from './pubsub/privateChat';
-import { listDatabases } from './demo';
-import { createUser, deleteUser, getAllUsers, getUserByUsername } from './controllers/users';
 
 const port = 3001;
-
-
-/*
- * Access variables in the .env file via process.env
-*/
-dotenv.config();
-
-
-/*
- * Connect to MongoDB database
-*/
-const uri = process.env.MONGODB_URI
-export const client = new MongoClient(uri)
-const main = async () => {
-  try {
-    await client.connect()
-    console.log('connected to MongoDB')
-
-    // await createUser(client, {
-    //   username: 'fennec'
-    // })
-
-    // await getUserByUsername(client, 'grogu')
-
-    // await getUserByUsername(client, 'din')
-
-    // await getUserByUsername(client, 'fennec')
-
-    // await deleteUser(client, 'fennec')
-
-    await getAllUsers(client)
-
-  } catch(e) {
-    console.log(e)
-  } finally {
-    await client.close()
-  }
-}
-
-main().catch(console.error)
 
 
 /*
