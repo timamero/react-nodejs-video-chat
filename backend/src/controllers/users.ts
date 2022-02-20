f3f7d710aa63d4748c53b63ecff3ab259478bd7a
Fatima Camero <fcamero12@gmail.com>
2022-02-19 17:02:10 -0800

    update deleteUser function

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -4,18 +4,15 @@ export async function createUser(client: MongoClient, newUser: Document) {
   const dbName = process.env.NODE_ENV === 'test' ? 'test' : 'chat'
   const result = await client.db(dbName).collection('users').insertOne(newUser)
 
-  console.log(`New user created with the following id: ${result.insertedId}`)
+  // console.log(`New user created with the following id: ${result.insertedId}`)
 }
 
 export async function deleteUser(client: MongoClient, username: string) {
-  const result = await client.db('chat').collection('users').findOneAndDelete({username})
-
-  if (result.value) {
-    console.log(`the user ${username} was deleted`)
-  } else {
-    console.log(`${username} was not deleted because it does not exist in database`)
-  }
+  const dbName = process.env.NODE_ENV === 'test' ? 'test' : 'chat'
+  const result = await client.db(dbName).collection('users').findOneAndDelete({username})
   
+  // Return user object that was deleted
+  return result.value
 }
 
 export async function getUserByUsername(client: MongoClient, username: string) {
