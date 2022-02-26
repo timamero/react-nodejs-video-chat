2b788dddb47f8b6d55c612e9a52541620b56a34b
Fatima Camero <fcamero12@gmail.com>
2022-02-25 20:31:19 -0800

    user data is saved to database when user enters

--- backend/src/pubsub/users.ts
+++ backend/src/pubsub/users.ts
@@ -1,7 +1,7 @@
 import { Server, Socket } from 'socket.io';
-import { getAllUsers } from '../controllers/users';
+// import { client } from '../database';
+import { getAllUsers, createUser } from '../controllers/users';
 
-// Will implement user authentication later, for now will save active users in users list
 interface User {
   id: string;
   username: string;
@@ -13,15 +13,15 @@ const user = (socket: Socket, io: Server) => {
   socket.on('user entered', async (username) => {
     console.log(`${username} has entered`);
 
-    // const users = await getAllUsers()
+    await createUser({socketId: socket.id, username: username});
 
-    if (!users.map(user => user.id).includes(socket.id)) {
-      users.push({id: socket.id, username: username});
+    // if (!users.map(user => user.id).includes(socket.id)) {
+    //   users.push({id: socket.id, username: username});
       
-      console.log('sending users', users);
-      io.emit('get user list', users);
-      io.to(socket.id).emit('get socket id', socket.id);
-    }
+    //   console.log('sending users', users);
+    //   io.emit('get user list', users);
+    //   io.to(socket.id).emit('get socket id', socket.id);
+    // }
   })
 
   socket.on('disconnect', () => {
