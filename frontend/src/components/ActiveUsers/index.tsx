f4bba06d39a16aa07c908857449e8ea71235305e
Fatima Camero <fcamero12@gmail.com>
2022-03-03 07:29:37 -0800

    reorganize ActiveUsers components

--- frontend/src/components/ActiveUsers/index.tsx
+++ frontend/src/components/ActiveUsers/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useAppSelector } from "../app/hooks";
+import { useAppSelector } from "../../app/hooks";
 import ActiveUserItem from "./ActiveUserItem";
 
 const ActiveUsers = () => {
