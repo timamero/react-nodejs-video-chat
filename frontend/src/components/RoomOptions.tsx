c126156c5b6eba28f3cdb9324e2e7f33dc5b2c6b
Fatima Camero <fcamero12@gmail.com>
2022-03-04 13:14:31 -0800

    set chat visibility state when button is clicked

--- frontend/src/components/RoomOptions.tsx
+++ frontend/src/components/RoomOptions.tsx
@@ -1,12 +1,14 @@
 import React, { useContext } from "react";
-import { useAppSelector } from '../app/hooks';
+import { setChatVisbility } from "../app/features/roomSlice";
+import { useAppSelector, useAppDispatch } from '../app/hooks';
 import { SocketContext } from "../context/socket";
 
 const RoomOptions = () => {
   const socket = useContext(SocketContext)
-  // const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch()
 
   const room = useAppSelector(state => state.room)
+  const isChatVisible = useAppSelector(state => state.room.isChatVisible)
 
   const handleEndClick = () => {
     console.log('end chat')
@@ -14,13 +16,13 @@ const RoomOptions = () => {
   }
 
   const handleToggleChat = () => {
-    console.log('toggle chat')
+    dispatch(setChatVisbility(!isChatVisible))
   }
 
   return (
     <div className="mt-4 mb-4 is-flex is-flex-direction-row is-justify-content-center">
       <button className="button is-danger mr-1" onClick={handleEndClick}>End Chat</button>
-      <button className="button is-primary mr-1" onClick={handleToggleChat}>Show Chat</button>
+      <button className="button is-primary mr-1" onClick={handleToggleChat}>{isChatVisible ? 'Hide Chat' : 'Show Chat'}</button>
     </div>
   )
 }
