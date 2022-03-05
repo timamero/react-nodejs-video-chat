5b5ad024a2f1d240825a8f858fcc1f719617cef3
Fatima Camero <fcamero12@gmail.com>
2022-03-05 12:17:25 -0800

    remove dev log

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -3,7 +3,6 @@ import { useLocation } from "react-router-dom";
 
 const Header = () => {
   const location = useLocation()
-  console.log('location header', location.pathname)
   return (
     <div id="header">
       {location.pathname === "/" && <h1 className="title is-1 has-text-centered">Chat App</h1>}
