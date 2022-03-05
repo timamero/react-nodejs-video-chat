14e8f8beeb1faefca56dc0667a7e428894887529
Fatima Camero <fcamero12@gmail.com>
2022-03-05 12:41:44 -0800

    move receive chat message listener to PrivateRoom component

--- frontend/src/components/MessagesDisplay/index.tsx
+++ frontend/src/components/MessagesDisplay/index.tsx
@@ -1,44 +1,15 @@
-import React, { useContext, useEffect, useCallback, useRef } from "react";
-import { SocketContext } from "../../context/socket";
+import React, { useEffect, useRef } from "react";
 import Message from './Message';
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
-import { addMessage } from "../../app/features/roomSlice";
+import { useAppSelector } from '../../app/hooks';
 
 const MessagesDisplay: React.FC = () => {
-  const dispatch = useAppDispatch()
-
-  const socket = useContext(SocketContext)
-  const userId = useAppSelector(state => state.user.socketId)
   const messages = useAppSelector(state => state.room.messages)
 
   const messageEndRef = useRef<null | HTMLDivElement>(null)
 
-  const generateRandomNum = () => {
-    return Math.floor((Math.random() * 10000))
-  }
-
   const scrollToBottom = () => {
     messageEndRef.current!.scrollIntoView({ behavior: 'smooth'})
   }
-
-  const updateMessages = useCallback((message) => {
-    dispatch(addMessage(message))
-  }, [dispatch])
-
-  useEffect(() => {
-    socket.once('receive chat message', ( messageData ) => {
-      console.log('received message')
-      const firstMessageClassName = messages.length === 0 ? 'mt-auto' : ''
-      const userClassName = messageData.userId === userId ? 'peer1Message' : 'peer2Message'
-      const newMessage = {
-        content: messageData.msg,
-        userId: messageData.userId,
-        className: `${firstMessageClassName} ${userClassName}`,
-        id: generateRandomNum()
-      }
-      updateMessages(newMessage)
-    })
-  }, [socket, messages, userId, updateMessages])
 
   useEffect(() => {
     scrollToBottom()
--- frontend/src/pages/PrivateRoom.tsx
+++ frontend/src/pages/PrivateRoom.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useCallback } from 'react';
 import Layout from '../components/Layout';
 import { setNotification, resetNotification } from '../app/features/notificationSlice';
 import { useAppSelector, useAppDispatch } from '../app/hooks';
@@ -8,18 +8,40 @@ import RoomOptions from '../components/RoomOptions';
 import { Navigate } from 'react-router-dom';
 import { SocketContext } from '../context/socket';
 import VideoDisplay from '../components/VideoDisplay';
+import { addMessage } from "../app/features/roomSlice";
+import { generateRandomNum } from '../util/helper';
 
 const PrivateRoom = () => {
   const socket = useContext(SocketContext)
   const dispatch = useAppDispatch()
+  
   const room = useAppSelector(state  => state.room)
   const userId = useAppSelector(state => state.user.socketId)
   const isChatVisible = useAppSelector(state => state.room.isChatVisible)
+  const messages = useAppSelector(state => state.room.messages)
 
   useEffect(() => {
     socket.removeAllListeners('enter chat room')
   }, [socket])
   const userHasAccess = room.users.includes(userId)
+
+  const updateMessages = useCallback((message) => {
+    dispatch(addMessage(message))
+  }, [dispatch])
+
+  useEffect(() => {
+    socket.once('receive chat message', ( messageData ) => {
+      const firstMessageClassName = messages.length === 0 ? 'mt-auto' : ''
+      const userClassName = messageData.userId === userId ? 'peer1Message' : 'peer2Message'
+      const newMessage = {
+        content: messageData.msg,
+        userId: messageData.userId,
+        className: `${firstMessageClassName} ${userClassName}`,
+        id: generateRandomNum()
+      }
+      updateMessages(newMessage)
+    })
+  }, [socket, messages, userId, updateMessages])
 
   if (!userHasAccess) {
     const notificationData = {
