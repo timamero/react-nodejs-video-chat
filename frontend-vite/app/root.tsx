c5ce473de85bc7f90550325dc7d5abfe332bc0bc
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-05 17:35:39 -0700

    Create root and route files

---
+++ frontend-vite/app/root.tsx
@@ -0,0 +1,16 @@
+import { Outlet, Scripts } from 'react-router';
+
+export default function App() {
+  return (
+    <html>
+      <head>
+        <link rel="icon" href="data:image/x-icon;base64,AA" />
+      </head>
+      <body>
+        <h1>Hello world!</h1>
+        <Outlet />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
