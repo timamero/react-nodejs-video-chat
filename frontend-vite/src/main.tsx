5983b52e2cba95bd9de98f18d659cfe3e468fd38
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-05-07 21:38:11 -0700

    Create route

--- frontend-vite/src/main.tsx
+++ frontend-vite/src/main.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter, Routes, Route } from 'react-router';
 
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 );
