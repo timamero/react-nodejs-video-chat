6f619dc9b481ea650af27813d635c94f07dafb69
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-07-16 02:38:52 -0700

    Update database to connect to redis

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,49 +1,51 @@
-import { MongoClient } from 'mongodb';
+import { createClient } from 'redis';
 
-let uri;
+let url: string;
 if (process.env.NODE_ENV === 'production') {
-  console.log('production: uri', process.env.MONGODB_URI_PROD);
-  uri = process.env.MONGODB_URI_PROD;
+  console.log('production: redis url', process.env.REDIS_URI_PROD);
+  url = process.env.REDIS_URI_PROD || 'redis://localhost:6739';
 } else {
   console.log('not production', process.env.NODE_ENV);
-  uri = process.env.MONGODB_URI;
+  url = process.env.REDIS_URL || 'redis://localhost:6739';
 }
-// const uri = process.env.MONGODB_URI;
 
 /*
- * Create the mongodb client
-*/
-export let client: MongoClient;
-if (process.env.NODE_ENV === 'test') {
-  const globalURI = global as typeof globalThis & {
-    __MONGO_URI__: string;
-  };
+ * Create the redis client and connect to the redis server
+ */
+export const client = createClient({
+  url,
+});
 
-  const options: MongoOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    keepAlive: true,
-  };
+client.on('error', (err) => console.log('Redis Client Error', err));
+// if (process.env.NODE_ENV === 'test') {
+//   const globalURI = global as typeof globalThis & {
+//     __MONGO_URI__: string;
+//   };
 
-  client= new MongoClient(globalURI.__MONGO_URI__, options);
-} else {
-  client = new MongoClient(uri);
-}
+//   const options: MongoOptions = {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     keepAlive: true,
+//   };
+
+//   client = new MongoClient(globalURI.__MONGO_URI__, options);
+// } else {
+//   client = new MongoClient(uri);
+// }
 
 /**
- * Connect mongodb client
+ * Connect to the redis server and clean the database on server restart
  */
 const main = async () => {
   try {
     await client.connect();
+    console.log('Connected to Redis');
 
     // clean database everytime the server restarts
-    await client.db('chat').collection('users').deleteMany({});
-    await client.db('chat').collection('room').deleteMany({});
-
-    console.log('Connected to MongoDB');
-  } catch(e) {
-    console.log(e);
+    await client.flushDb();
+    console.log('Redis database cleaned');
+  } catch (e) {
+    console.error('Failed to connect to Redis', e);
   }
 };
 
