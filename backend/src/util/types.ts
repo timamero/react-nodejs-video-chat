1641c2357b693e601e8f3a7ee66c0ce8290becb4
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-07-16 20:45:16 -0700

    Clean up

--- backend/src/util/types.ts
+++ backend/src/util/types.ts
@@ -6,12 +6,8 @@
  */
 export interface Room {
   id: string;
-  users: User[];
+  users: string;
 }
-// export interface Room {
-//   _id: ObjectId;
-//   users: User[];
-// }
 
 export interface User {
   id: string;
@@ -20,8 +16,3 @@ export interface User {
   isBusy: string;
   [key: string]: any; // To allow other dynamic fields
 }
-// export interface User {
-//   _id: ObjectId;
-//   username: string;
-//   socketId: string;
-// }
