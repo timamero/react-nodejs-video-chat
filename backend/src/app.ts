3ca32fd02ed602eef13c7b54b129c9f3cefc6e2b
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-07-17 10:40:06 -0700

    Connect to redis

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,21 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-dotenv.config();
-import connectDatabase from './database';
+import connectRedis from './database';
 
 const app = express();
 app.use(cors());
 
 /*
  * Access variables in the .env file via process.env
-*/
+ */
 dotenv.config();
 
-
 /*
- * Connect to MongoDB database
-*/
-connectDatabase().catch(console.error);
+ * Connect to redis database
+ */
+connectRedis();
 
 export default app;
